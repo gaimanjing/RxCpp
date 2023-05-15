2042662fefa254fd26ab9f4644a769fd51368c8f
Guanhua Hou <gaimanjing@gmail.com>
2023-05-15 16:43:14 +0800

    added: observe_on unit test

--- test/play_test.cpp
+++ test/play_test.cpp
@@ -8,7 +8,7 @@ protected:
   void TearDown() override {}
 };
 
-TEST_F(PlayTestSuite, Thread_safe_1) {
+TEST_F(PlayTestSuite, Thread_safe_1_subscribe) {
   // Given
   auto values1 = rxcpp::observable<>::range(1, 5);
   auto ret = false;
@@ -39,9 +39,9 @@ TEST_F(PlayTestSuite, Thread_safe_1) {
   EXPECT_TRUE(ret);
 }
 
-TEST_F(PlayTestSuite, Thread_safe_2) {
+TEST_F(PlayTestSuite, Thread_safe_2_subscribe_on) {
   // Given
-  auto values1 = rxcpp::observable<>::range(1, 5);
+  auto values1 = rxcpp::observable<>::range(1, 2);
   auto ret = false;
 
   auto threads = rxcpp::observe_on_new_thread();
@@ -70,6 +70,51 @@ TEST_F(PlayTestSuite, Thread_safe_2) {
 
   auto threads3 = rxcpp::observe_on_new_thread();
   values1.subscribe_on(threads3).as_blocking().subscribe(
+      [](int v) {
+        printf("OnNext, 3: %d\n", v);
+        printf("");
+      },
+      [&]() {
+        printf("OnCompleted, 3\n");
+        printf("");
+        ret = true;
+      });
+
+  // Then
+  EXPECT_TRUE(ret);
+}
+
+TEST_F(PlayTestSuite, Thread_safe_3_observe_on) {
+  // Given
+  auto values1 = rxcpp::observable<>::range(1, 2);
+  auto ret = false;
+
+  auto threads = rxcpp::observe_on_new_thread();
+
+  // When
+  values1.observe_on(threads).subscribe(
+      [](int v) {
+        printf("OnNext, 1: %d\n", v);
+        printf("");
+      },
+      []() {
+        printf("OnCompleted, 1\n");
+        printf("");
+      });
+
+  auto threads2 = rxcpp::observe_on_new_thread();
+  values1.observe_on(threads2).subscribe(
+      [](int v) {
+        printf("OnNext, 2: %d\n", v);
+        printf("");
+      },
+      [&]() {
+        printf("OnCompleted, 2\n");
+        printf("");
+      });
+
+  auto threads3 = rxcpp::observe_on_new_thread();
+  values1.observe_on(threads3).as_blocking().subscribe(
       [](int v) {
         printf("OnNext, 3: %d\n", v);
         printf("");
