bd3ac4d1145ec006cfc247bf566cd7846d23175f
Guanhua Hou <gaimanjing@gmail.com>
2023-05-16 17:02:48 +0800

    added: observe_on_run_loop, observe_on_event_loop,  test

--- test/play_test.cpp
+++ test/play_test.cpp
@@ -8,7 +8,7 @@ protected:
   void TearDown() override {}
 };
 
-TEST_F(PlayTestSuite, Thread_safe_1_subscribe) {
+TEST_F(PlayTestSuite, subscribe) {
   // Given
   auto values1 = rxcpp::observable<>::range(1, 5);
   auto ret = false;
@@ -39,7 +39,7 @@ TEST_F(PlayTestSuite, Thread_safe_1_subscribe) {
   EXPECT_TRUE(ret);
 }
 
-TEST_F(PlayTestSuite, Thread_safe_2_subscribe_on) {
+TEST_F(PlayTestSuite, subscribe_on_new_thread) {
   // Given
   auto values1 = rxcpp::observable<>::range(1, 2);
   auto ret = false;
@@ -84,7 +84,7 @@ TEST_F(PlayTestSuite, Thread_safe_2_subscribe_on) {
   EXPECT_TRUE(ret);
 }
 
-TEST_F(PlayTestSuite, Thread_safe_3_observe_on) {
+TEST_F(PlayTestSuite, observe_on_new_thread) {
   // Given
   auto values1 = rxcpp::observable<>::range(1, 2);
   auto ret = false;
@@ -124,6 +124,75 @@ TEST_F(PlayTestSuite, Thread_safe_3_observe_on) {
         printf("");
         ret = true;
       });
+
+  // Then
+  EXPECT_TRUE(ret);
+}
+
+TEST_F(PlayTestSuite, observe_on_run_loop) {
+  // Given
+  rxcpp::subjects::subject<int> a_subject;
+  auto values1 = a_subject.get_observable();
+
+  auto main_run_loop = rxcpp::schedulers::run_loop();
+  auto threads = rxcpp::observe_on_run_loop(main_run_loop);
+
+  auto ret = false;
+
+  values1.observe_on(threads).subscribe(
+      [](int v) {
+        printf("OnNext, 1: %d\n", v);
+        printf("");
+      },
+      [&]() {
+        printf("OnCompleted, 1\n");
+        printf("");
+        ret = true;
+      });
+
+  // When
+  a_subject.get_subscriber().on_next(1);
+  a_subject.get_subscriber().on_next(2);
+  a_subject.get_subscriber().on_completed();
+
+  while (!main_run_loop.empty()) {
+    main_run_loop.dispatch();
+  }
+
+  // Then
+  EXPECT_TRUE(ret);
+}
+
+TEST_F(PlayTestSuite, observe_on_event_loop) {
+  // Given
+  rxcpp::subjects::subject<int> a_subject;
+  rxcpp::subjects::subject<int> block_subject;
+  auto values1 = a_subject.get_observable();
+
+  auto threads = rxcpp::observe_on_event_loop();
+
+  auto ret = false;
+
+  values1.observe_on(threads).subscribe(
+      [](int v) {
+        // run in new thread
+        printf("OnNext, 1: %d\n", v);
+        printf("");
+      },
+      [&]() {
+        // run in new thread
+        printf("OnCompleted, 1\n");
+        printf("");
+        ret = true;
+        block_subject.get_subscriber().on_completed();
+      });
+
+  // When
+  a_subject.get_subscriber().on_next(1);
+  a_subject.get_subscriber().on_next(2);
+  a_subject.get_subscriber().on_completed();
+
+  block_subject.get_observable().as_blocking().subscribe();
 
   // Then
   EXPECT_TRUE(ret);
