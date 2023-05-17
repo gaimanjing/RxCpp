5cc811c5ab5e6345051983e95878811bddb5de9a
Guanhua Hou <gaimanjing@gmail.com>
2023-05-17 13:16:15 +0800

    updated: unit test

--- test/play_test.cpp
+++ test/play_test.cpp
@@ -197,3 +197,165 @@ TEST_F(PlayTestSuite, observe_on_event_loop) {
   // Then
   EXPECT_TRUE(ret);
 }
+
+TEST_F(PlayTestSuite, serialize_one_worker) {
+  // Given
+  rxcpp::subjects::subject<int> a_subject;
+  rxcpp::subjects::subject<int> block_subject;
+  auto values1 = a_subject.get_observable();
+
+  // auto threads = rxcpp::serialize_event_loop();
+  // auto threads = rxcpp::serialize_new_thread();
+  // auto threads = rxcpp::serialize_same_worker();
+
+  auto threads = rxcpp::synchronize_event_loop();
+
+  auto ret = false;
+
+  values1.observe_on(threads).subscribe(
+      [](int v) {
+        // run in new thread
+        printf("OnNext, 1: %d\n", v);
+        printf("");
+      },
+      [&]() {
+        // run in new thread
+        printf("OnCompleted, 1\n");
+        printf("");
+        ret = true;
+        block_subject.get_subscriber().on_completed();
+      });
+
+  // When
+  a_subject.get_subscriber().on_next(1);
+  a_subject.get_subscriber().on_next(2);
+  a_subject.get_subscriber().on_completed();
+
+  block_subject.get_observable().as_blocking().subscribe();
+
+  // Then
+  EXPECT_TRUE(ret);
+}
+
+TEST_F(PlayTestSuite, SchedulerOne) {
+  //---------- Get a Coordination
+  auto coordination = rxcpp::serialize_new_thread();
+
+  //------- Create a Worker instance
+  auto worker = coordination.create_coordinator().get_worker();
+
+  auto sub_action = rxcpp::schedulers::make_action(
+      [](const rxcpp::schedulers::schedulable &) {
+        std::cout << "Action Executed in Thread # : "
+                  << std::this_thread::get_id() << std::endl;
+      });
+
+  auto scheduled = rxcpp::schedulers::make_schedulable(worker, sub_action);
+  scheduled.schedule();
+}
+
+TEST_F(PlayTestSuite, SchedulerTwo) {
+  std::cout << "#0 Thread #" << std::this_thread::get_id() << std::endl;
+  auto coordination = rxcpp::identity_current_thread();
+  // auto coordination = rxcpp::observe_on_new_thread();
+
+  auto worker = coordination.create_coordinator().get_worker();
+  auto start = coordination.now() + std::chrono::milliseconds(1);
+  auto period = std::chrono::milliseconds(1);
+  auto values = rxcpp::observable<>::interval(start, period)
+                    .take(5)
+                    .replay(2, coordination);
+
+  worker.schedule([&](const rxcpp::schedulers::schedulable &) {
+    values.subscribe(
+
+        [](long v) {
+          std::cout << "#1 Thread #" << std::this_thread::get_id()
+                    << "; on_next: " << v << std::endl;
+        },
+
+        []() { printf("#1 --- OnCompleted\n"); });
+  });
+
+  worker.schedule([&](const rxcpp::schedulers::schedulable &) {
+    values.subscribe(
+
+        [](long v) {
+          std::cout << "#2 Thread #" << std::this_thread::get_id()
+                    << "; on_next: " << v << std::endl;
+        },
+
+        []() { printf("#2 --- OnCompleted\n"); });
+  });
+
+  // Start emitting
+
+  worker.schedule(
+      [&](const rxcpp::schedulers::schedulable &) { values.connect(); });
+
+  // Add blocking subscription to see results
+
+  values.as_blocking().subscribe();
+}
+
+TEST_F(PlayTestSuite, subject_no_subscriber) {
+  // Given
+  rxcpp::subjects::subject<int> a_subject;
+  auto values1 = a_subject.get_observable();
+
+  // When
+  a_subject.get_subscriber().on_next(1);
+  a_subject.get_subscriber().on_next(2);
+  a_subject.get_subscriber().on_completed();
+
+  // Then
+}
+
+TEST_F(PlayTestSuite, subject) {
+  // Given
+  rxcpp::subjects::subject<int> a_subject;
+  auto values1 = a_subject.get_observable();
+
+  auto ret = false;
+  values1.subscribe(
+      [](int v) {
+        printf("OnNext, 1: %d\n", v);
+        printf("");
+      },
+      [&]() {
+        printf("OnCompleted, 1\n");
+        printf("");
+        ret = true;
+      });
+
+  // When
+  a_subject.get_subscriber().on_next(1);
+  a_subject.get_subscriber().on_next(2);
+  a_subject.get_subscriber().on_completed();
+
+  // Then
+  EXPECT_TRUE(ret);
+}
+
+TEST_F(PlayTestSuite, range_map_observe_on) {
+  //---------------- Generate a range of values
+  //---------------- Apply Square function
+  auto values =
+      rxcpp::observable<>::range(1, 4).map([](int v) { return v * v; });
+
+  //------------- Emit the current thread details
+  std::cout << "Main Thread id => " << std::this_thread::get_id() << std::endl;
+  //---------- observe_on another thread....
+  //---------- make it blocking to
+  values.observe_on(rxcpp::synchronize_new_thread())
+      .as_blocking()
+      .subscribe(
+          [](int v) {
+            std::cout << "Observable Thread id => "
+                      << std::this_thread::get_id() << "  " << v << std::endl;
+          },
+          []() { std::cout << "OnCompleted" << std::endl; });
+
+  //------------------ Print the main thread details
+  std::cout << "Main Thread id => " << std::this_thread::get_id() << std::endl;
+}
